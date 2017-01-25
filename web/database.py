1bc0bd324c0fce3b44381cd73ca22790ff995a3a
kevthehermit <kevthehermit@gmail.com>
2017-01-25 21:40:57 +0000

    debug prints

--- web/database.py
+++ web/database.py
@@ -40,7 +40,7 @@ class Database():
             print 'Error', e
 
     def get_allsessions(self, start=0, length=25, search_term=None, col_name='starttime', order=1):
-        print a
+        print "a"
 
         if search_term and col_name:
             cursor = self.col_sessions.find({col_name: {"$regex": u"{}".format(search_term)}})
@@ -53,7 +53,7 @@ class Database():
             cursor.sort(col_name, pymongo.DESCENDING)
         sessions = cursor.skip(start).limit(length)
 
-        print b
+        print "b"
         return [s for s in sessions]
 
     def get_session(self, search):

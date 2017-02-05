336d9cc0bd7d5ed84d881426385b12a8ca3bf796
kevthehermit <kevthehermit@gmail.com>
2017-02-05 17:32:13 +0000

    Create Docker File

---
+++ utils/docker/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo Configuring Mollusc
+# edit the config file with ENV
+cp /opt/mollusc/mollusc.conf.sample /opt/mollusc/mollusc.conf
+# Set Mongo URI
+sed -i "s|mongodb://localhost|$MONGO_URI|g" /opt/mollusc/mollusc.conf
+# Set auth to enabled
+sed -i "s|enable = False|enable = $AUTH|g" /opt/mollusc/mollusc.conf
+# Set Maps Key
+sed -i "s|mapskey|$MAP_KEY|g" /opt/mollusc/mollusc.conf
+echo Printing Config File to screen
+cat /opt/mollusc/mollusc.conf
+echo
+echo Starting Mollusc
+chown -R www-data:www-data /opt/mollusc
+service apache2 restart
+
+
+# Tail out the log file
+tail -f /opt/mollusc/mollusc.log
